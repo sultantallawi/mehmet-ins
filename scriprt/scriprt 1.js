e5f17801dfe5231bf264638735cde3a1956f8b74
Sultan Tallawi <sultan.tallawi@gmail.com>
2025-11-08 19:47:25 +0300

    Add files via upload

---
+++ scriprt/scriprt 1.js
@@ -0,0 +1,11 @@
+// 🔘 زر القائمة في الموبايل
+const navToggle = document.getElementById("navToggle");
+const navMenu = document.getElementById("navMenu");
+
+if (navToggle && navMenu) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("open");
+  });
+}
+
+//a
